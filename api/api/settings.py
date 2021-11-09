462f455066fbe32d4254ee1b0f72348d0e717968
seokzin <seokzin95@gmail.com>
2021-11-09 11:13:14 +0900

    wip: Add cors middleware

--- api/api/settings.py
+++ api/api/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleward.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
