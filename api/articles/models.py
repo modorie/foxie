73ba45c3b5d8a21d874181b45839bc55bc3c1bc2
seokzin <seokzin95@gmail.com>
2021-11-09 16:46:00 +0900

    wip: Add spaces

--- api/articles/models.py
+++ api/articles/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+
+
 class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
 class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
     content = models.CharField(max_length=200)
