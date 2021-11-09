3179139d966934d42da836f31d74ebae79fe376b
seokzin <seokzin95@gmail.com>
2021-11-09 11:32:44 +0900

    feat: Add articles REST API

--- api/articles/views.py
+++ api/articles/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def article_create_or_list(request):
+    pass
+    
+
+def article_detail_or_update_or_delete(request, article_pk):
+    pass
+
+
+def comment_create(request, article_pk):
+    pass
+
+
+def comment_update_or_delete(request, article_pk, comment_pk):
+    pass
