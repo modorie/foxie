1c574612a6b7d7ed2bbb6e343b435aca27c84f83
seokzin <seokzin95@gmail.com>
2021-11-09 11:33:33 +0900

    feat: Add articles models

--- api/articles/models.py
+++ api/articles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
+    content = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
