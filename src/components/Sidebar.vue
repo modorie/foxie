d58ed6132893c534b21ca79e7ae48cb34764ff1a
seokzin <seokzin95@gmail.com>
2021-11-11 15:52:38 +0900

    feat: Add logo svg component

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,12 +1,23 @@
 <template>
-  <div class="container">
+  <div class="sidebar">
     <div class="control"></div>
+
+    <!-- TODO : for문으로 리팩토링 -->
+    <icon-base
+      viewBox="0 0 64 64"
+      width="64"
+      height="64"
+      iconColor="transparent"
+      icon-name="icon"
+    >
+      <icon-logo />
+    </icon-base>
 
     <icon-base
       viewBox="0 0 32 32"
       width="24"
       height="24"
-      iconColor="#ffffff"
+      iconColor="transparent"
       icon-name="icon"
     >
       <icon-home />
@@ -16,7 +27,7 @@
       viewBox="0 0 32 32"
       width="24"
       height="24"
-      iconColor="#ffffff"
+      iconColor="transparent"
       icon-name="droplet"
     >
       <icon-movie />
@@ -26,7 +37,7 @@
       viewBox="0 0 32 32"
       width="24"
       height="24"
-      iconColor="#ffffff"
+      iconColor="transparent"
       icon-name="droplet"
     >
       <icon-review />
@@ -36,7 +47,7 @@
       viewBox="0 0 32 32"
       width="24"
       height="24"
-      iconColor="#ffffff"
+      iconColor="transparent"
       icon-name="droplet"
     >
       <icon-community />
@@ -46,6 +57,7 @@
 
 <script>
 import IconBase from "./IconBase.vue";
+import IconLogo from "./icons/IconLogo.vue";
 import IconCommunity from "./icons/IconCommunity.vue";
 import IconHome from "./icons/IconHome.vue";
 import IconMovie from "./icons/IconMovie.vue";
@@ -54,6 +66,7 @@ import IconReview from "./icons/IconReview.vue";
 export default {
   name: "Sidebar",
   components: {
+    IconLogo,
     IconBase,
     IconHome,
     IconCommunity,
@@ -63,4 +76,18 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.sidebar {
+  background-color: #2f3641;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  bottom: 0;
+
+  width: 100px;
+
+  /* display: flex;
+  flex-direction: column; */
+}
+</style>
