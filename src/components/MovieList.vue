c59955a0e96a801a494fea25af0a65c2310d3498
seokzin <seokzin95@gmail.com>
2021-11-15 16:00:07 +0900

    feat: Add MovieList component

---
+++ src/components/MovieList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <MovieCard
+      v-for="movie in movies"
+      :key="movie.id"
+      :movie="movie"
+    ></MovieCard>
+  </div>
+</template>
+
+<script>
+import MovieCard from "./MovieCard.vue";
+
+export default {
+  name: "MovieList",
+  components: {
+    MovieCard,
+  },
+  props: {
+    movies: {
+      type: Array,
+    },
+    group_title: {
+      type: String,
+    },
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+  created() {
+    console.log(this.movies);
+  },
+};
+</script>
+
+<style scoped>
+/* TODO : 반응형이 이상하게 됨.. 공부해서 리팩토링 */
+.container {
+  @apply grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grid-cols-6;
+  gap: 20px 20px;
+}
+</style>
