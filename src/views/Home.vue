365ec8e26c40d8b2029f1ae2958adfa77924001e
seokzin <seokzin95@gmail.com>
2021-11-18 17:33:45 +0900

    feat: Add top banner

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,101 @@
 <template>
-  <div class="home">
-    <header>인덱스 페이지</header>
+  <div class="container">
+    <div class="banner">
+      <div class="banner__left" />
+      <div class="banner__right">
+        <div class="banner__right__brand">
+          <icon-base viewBox="0 0 43 54" width="42px" height="48px">
+            <icon-logo />
+          </icon-base>
 
-    <section>최신 글</section>
+          <p class="banner__right__title mulish">Foxie</p>
+        </div>
 
-    <div v-for="article in articles" :key="article.id">
-      <h2>{{ article.title }}</h2>
-      <p>{{ article.content }}</p>
+        <p class="banner__right__text">
+          난 네게 아직 수십 만의 영화에 지나지 않아,<br />
+          너 역시 내게 수십 만의 아이들과 같은 어린아이일뿐이야<br />
+          하지만 네가 나를 길들인다면 나는 너에게 이 세상에 유일한 존재가
+          될거야<br />
+          <br />
+          <b>널 길들이려면 어떻게 해야 하니?</b><br />
+        </p>
+        <div class="banner__footer">
+          <router-link to="#">
+            <div class="banner__footer__button">네 이야기를 들려주렴</div>
+          </router-link>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
-<script lang="js">
-// ts 이슈 해결
-import axios from "axios";
+<script>
+import IconBase from "@/components/IconBase.vue";
+import IconLogo from "@/components/icons/IconLogo.vue";
 
 export default {
-  name: "Home",
-
-  data() {
-    return {
-      Articles: [],
-    };
-  },
-  mounted() {
-    this.getArticles();
-  },
-  methods: {
-    getArticles() {
-      axios
-        .get("/api/v1/articles/")
-        .then((response) => console.log(response))
-        .catch((error) => {
-          console.log(error);
-        });
-    },
+  components: {
+    IconBase,
+    IconLogo,
   },
 };
 </script>
+
+<style>
+.banner {
+  width: 100%;
+  height: 20rem;
+  border-radius: 8px;
+  display: flex;
+  background: rgba(255, 255, 255, 0.65);
+  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.05);
+  backdrop-filter: blur(50px);
+}
+
+.banner__left {
+  background-image: url("https://ae01.alicdn.com/kf/H9da62382ce944506bfe2ee949239bc67K.jpg");
+  background-size: 100%;
+  width: 18rem;
+  height: 100%;
+  border-radius: 8px 0 0 8px;
+}
+
+.banner__right {
+  padding: 2rem;
+  width: 100%;
+}
+
+.banner__right__brand {
+  display: flex;
+}
+
+.banner__right__title {
+  font-size: 2rem;
+  margin-left: 1rem;
+  margin-bottom: 2rem;
+  color: #ed5656;
+  font-weight: 700;
+}
+
+.banner__right__text {
+  color: var(--header);
+  font-size: 16px;
+}
+
+.banner__footer {
+  width: 100%;
+  display: flex;
+  flex-direction: row-reverse;
+  margin-top: 1rem;
+}
+
+.banner__footer__button {
+  align-items: center;
+  text-align: center;
+  background-color: var(--btn-primary);
+  border: 1px solid var(--btn-primary);
+  color: var(--white);
+  padding: 0.5rem 1.5rem;
+  border-radius: 6px;
+}
+</style>
