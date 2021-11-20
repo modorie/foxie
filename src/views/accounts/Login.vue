3a97eaddda3ef7faff764bd803a35fb70030cb61
seokzin <seokzin95@gmail.com>
2021-11-20 23:06:01 +0900

    design: Login page (temp)

--- src/views/accounts/Login.vue
+++ src/views/accounts/Login.vue
@@ -1,28 +1,60 @@
 <template>
-  <div>
-    <h1>Login</h1>
-    <form @submit.prevent="login">
-      <div>
-        <label for="email">email: </label>
-        <input type="text" id="email" v-model="email" />
+  <div class="fixed">
+    <div class="container">
+      <div class="logo">
+        <router-link to="/">
+          <icon-base viewBox="0 0 43 54" width="6rem" height="6rem">
+            <icon-logo />
+          </icon-base>
+        </router-link>
       </div>
-      <div>
-        <label for="password">비밀번호: </label>
-        <input
-          type="password"
-          id="password"
-          v-model="password"
-          @keyup.enter="login"
-        />
+
+      <h1 class="login__text mulish">Log in to Foxie</h1>
+
+      <div class="card">
+        <form @submit.prevent="login">
+          <div>
+            <p class="login__label mulish">E-mail</p>
+            <input class="login__input" type="text" v-model="email" />
+          </div>
+
+          <div>
+            <p class="login__label mulish">Password</p>
+            <input
+              class="login__input"
+              type="password"
+              v-model="password"
+              @keyup.enter="login"
+            />
+          </div>
+
+          <router-link to="/">
+            <div class="login__button mulish" @click="login">Log in</div>
+          </router-link>
+        </form>
       </div>
-      <button @click="login">로그인</button>
-    </form>
+
+      <div class="login__footer">
+        <p>
+          아이디가 없으신가요?
+          <span class="sign__up mulish">
+            <router-link to="/signup"> Sign up </router-link></span
+          >
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import IconBase from "@/components/IconBase.vue";
+import IconLogo from "@/components/icons/IconLogo.vue";
+
 export default {
-  name: "Login",
+  components: {
+    IconBase,
+    IconLogo,
+  },
   data() {
     return {
       email: "",
@@ -46,3 +78,71 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.fixed {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: 101;
+  background-color: var(--background);
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.login__text {
+  color: var(--text);
+  font-size: 28px;
+  font-weight: 200;
+  margin-top: 2rem;
+  margin-bottom: 1rem;
+}
+
+.card {
+  width: 20rem;
+  background-color: var(--header);
+  box-shadow: 0px 1px 2px 1px rgba(0, 0, 0, 0.06);
+  padding: 2rem 1rem;
+  border-radius: 8px;
+  margin-bottom: 2rem;
+}
+
+.login__label {
+  margin-bottom: 0.2rem;
+  font-weight: 200;
+  padding-left: 0.3rem;
+  font-size: 14px;
+}
+
+.login__input {
+  background-color: var(--background);
+  border: 1px solid var(--board-header);
+  margin-bottom: 1.5rem;
+  border-radius: 4px;
+  width: 100%;
+  height: 2.5rem;
+  padding: 0.5rem;
+}
+
+.login__button {
+  width: 100%;
+  background-color: var(--coral);
+  border-radius: 8px;
+  text-align: center;
+  margin-top: 1.5rem;
+  padding: 0.5rem;
+  color: var(--white);
+  font-weight: 700;
+}
+
+.sign__up {
+  margin-left: 0.5rem;
+  color: var(--coral);
+  font-weight: 600;
+}
+</style>
