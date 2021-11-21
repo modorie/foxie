808ca080991a13f3d6c03833abd13822e014aba3
seokzin <seokzin95@gmail.com>
2021-11-21 20:38:35 +0900

    design: Update logo svg

--- src/components/icons/IconLogo.vue
+++ src/components/icons/IconLogo.vue
@@ -2,21 +2,21 @@
   <svg>
     <path
       d="M42 1V31.7341M42 1L21.5 16.367M42 1L32.6475 24.7234M42 31.7341H1M42 31.7341L32.6475 24.7234M42 31.7341L21.5 53M42 31.7341H29.8837M1 31.7341L21.5 53M1 31.7341V1M1 31.7341L10.3525 24.7234M1 31.7341H13.1163M21.5 53L32.6475 24.7234M21.5 53L10.3525 24.7234M21.5 53L29.8837 31.7341M21.5 53L13.1163 31.7341M1 1L10.3525 24.7234M1 1L21.5 16.367M10.3525 24.7234L21.5 16.367M10.3525 24.7234L13.1163 31.7341M21.5 16.367L32.6475 24.7234M32.6475 24.7234L29.8837 31.7341M29.8837 31.7341H13.1163"
-      stroke="url(#paint0_linear_143_25331)"
-      stroke-width="2.5"
+      stroke="url(#paint0_linear_128_14304)"
+      stroke-width="2"
       stroke-linejoin="round"
+      stroke-linecap="round"
     />
     <defs>
       <linearGradient
-        id="paint0_linear_143_25331"
+        id="paint0_linear_128_14304"
         x1="21.5"
         y1="1"
         x2="21.5"
         y2="53"
         gradientUnits="userSpaceOnUse"
       >
-        <stop stop-color="#F87096" />
-        <stop offset="0.0001" stop-color="#EC6159" />
+        <stop stop-color="#EC6159" />
         <stop offset="1" stop-color="#FDAF88" />
       </linearGradient>
     </defs>
