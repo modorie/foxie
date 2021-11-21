0be0c0de3ae4f5151b5134083411b64a8c31e402
seokzin <seokzin95@gmail.com>
2021-11-22 02:11:02 +0900

    wip: Add svg icon components

---
+++ src/components/icons/IconComment.vue
@@ -0,0 +1,15 @@
+<template>
+  <path
+    d="M6 7H6.01M10 7H10.01M14 7H14.01M7 13H3C1.89543 13 1 12.1046 1 11V3C1 1.89543 1.89543 1 3 1H17C18.1046 1 19 1.89543 19 3V11C19 12.1046 18.1046 13 17 13H12L7 18V13Z"
+    stroke="#111827"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
