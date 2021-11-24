b78127accd9494750ca2e0312f31e6a50ee97fd2
seokzin <seokzin95@gmail.com>
2021-11-24 14:26:02 +0900

    wip: Add X svg component

---
+++ src/components/icons/IconX.vue
@@ -0,0 +1,15 @@
+<template>
+  <path
+    d="M6 18L18 6M6 6L18 18"
+    stroke="var(--recommend-text)"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
