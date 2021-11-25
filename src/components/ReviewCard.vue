2b1e3f2acb959735c034060755e1867d36964497
devpla <chaeyeonhee@kakao.com>
2021-11-25 15:11:00 +0900

    feat: Link review feed page with server

--- src/components/ReviewCard.vue
+++ src/components/ReviewCard.vue
@@ -1,46 +1,107 @@
 <template>
   <div class="container">
     <div class="profile">
-      <div class="profile__avatar">
-        <icon-base viewBox="0 0 64 64" width="48" height="48">
-          <icon-avatar />
-        </icon-base>
-      </div>
+      <router-link
+        :to="{ name: 'Profile', params: { username: review.author.username } }"
+      >
+        <div class="profile__avatar">
+          <img
+            v-if="review.author.profile.avatar"
+            :src="review.author.profile.avatar"
+            style="height: 48px; width: 48px"
+            class="settings__form__photo__thumbnail"
+          />
+          <icon-base v-else viewBox="0 0 64 64" width="48" height="48">
+            <icon-avatar />
+          </icon-base>
+        </div>
+      </router-link>
 
       <div class="profile__info">
         <div class="profile__info__header">
-          <p class="profile__nickname">4시의여우</p>
-          <p class="profile__time">9시간 전</p>
+          <div class="profile__info__header__user">
+            <router-link
+              :to="{
+                name: 'Profile',
+                params: { username: review.author.username },
+              }"
+            >
+              <p
+                class="profile__nickname"
+                v-text="
+                  review.author.profile.nickname
+                    ? review.author.profile.nickname
+                    : review.author.username
+                "
+              ></p>
+            </router-link>
+            <p class="profile__time">{{ review.created_at.slice(0, 10) }}</p>
+          </div>
+          <div v-if="review.author.id === userId" class="profile__toggle">
+            <button
+              @click="isDropdownOpen = !isDropdownOpen"
+              class="profile__toggle__button"
+            >
+              <icon-base viewBox="0 0 22 26" width="24" height="22">
+                <icon-horizontal-dots class="dots__svg" />
+              </icon-base>
+            </button>
+            <transition name="fade">
+              <ReviewDropdown
+                v-if="isDropdownOpen"
+                @close-dropdown="isDropdownOpen = false"
+                :review="review"
+                :reviewId="review.id"
+              />
+            </transition>
+          </div>
         </div>
 
         <!-- TODO : 컴포넌트로 분리해서 점수만 줬을 때 별점 만들어지도록 할 예정 -->
         <div class="profile__score">
-          <StarRating size="24" score="9" />
+          <StarRating size="18" :score="review.rank" />
         </div>
       </div>
     </div>
     <div class="body">
-      <p class="body__content" v-if="!isMore">
-        {{ text | truncate(200) }}
+      <p class="body__content" v-if="(review.content.length >= 120) & !isMore">
+        {{ review.content | truncate(120) }}
       </p>
 
       <p class="body__content" v-else>
-        {{ text }}
+        {{ review.content }}
       </p>
 
-      <div class="body__more" v-if="!isMore" @click="isMore = !isMore">
+      <div
+        class="body__more"
+        v-if="(review.content.length >= 200) & !isMore"
+        @click="isMore = !isMore"
+      >
         더보기
       </div>
+      <div
+        class="body__more"
+        v-if="(review.content.length >= 200) & isMore"
+        @click="isMore = !isMore"
+      >
+        간략히
+      </div>
 
-      <ReviewMovieCard />
+      <ReviewMovieCard :movieId="review.movie" />
 
       <div class="footer">
         <div class="footer__item">
-          <icon-base viewBox="0 0 22 25" width="24" height="22">
-            <icon-heart class="footer__svg" />
-          </icon-base>
-
-          <p class="footer__item__count">17</p>
+          <button @click="likeIt(review.id)">
+            <icon-base
+              viewBox="0 0 22 25"
+              width="24"
+              height="22"
+              :iconColor="[isLike ? 'var(--coral)' : 'var(--heart)']"
+            >
+              <icon-heart class="footer__svg like__svg" />
+            </icon-base>
+          </button>
+          <p class="footer__item__count">{{ likeCount }}</p>
         </div>
 
         <div class="footer__item">
@@ -48,12 +109,13 @@
             <icon-comment class="footer__svg" />
           </icon-base>
 
-          <p class="footer__item__count">17</p>
+          <p class="footer__item__count">{{ review.comments.length }}</p>
         </div>
       </div>
 
+      <ReviewCommentWrite :review="propReview" />
       <div class="comment">
-        <div class="comment__avatar">
+        <!-- <div class="comment__avatar">
           <icon-base viewBox="0 0 64 64" width="32" height="32">
             <icon-avatar />
           </icon-base>
@@ -63,47 +125,96 @@
           class="footer__input"
           placeholder="댓글을 입력하세요."
           type="text"
-        />
+        /> -->
       </div>
+      <ReviewComment
+        v-for="comment in review.comments"
+        :key="comment.id"
+        :review="review"
+        :commentId="comment.id"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 import ReviewMovieCard from "@/components/ReviewMovieCard.vue";
+import ReviewCommentWrite from "@/components/ReviewCommentWrite.vue";
+import ReviewComment from "@/components/ReviewComment.vue";
+import ReviewDropdown from "@/components/ReviewDropdown.vue";
 
 import IconBase from "@/components/IconBase.vue";
 import IconAvatar from "@/components/icons/IconAvatar.vue";
 import IconHeart from "@/components/icons/IconHeart.vue";
 import IconComment from "@/components/icons/IconComment.vue";
+import IconHorizontalDots from "@/components/icons/IconHorizontalDots.vue";
 import StarRating from "@/components/StarRating.vue";
 
 export default {
   components: {
     ReviewMovieCard,
+    ReviewCommentWrite,
+    ReviewComment,
+    ReviewDropdown,
     IconBase,
     IconAvatar,
     IconHeart,
     IconComment,
+    IconHorizontalDots,
     StarRating,
   },
   data() {
     return {
-      text: `언론 시사회에서 처음 이터널스를 보고 접하는 느낌은 딱 하나였다. "이
-        영화는 마블판 스타워즈: 라스트 제다이(2017년)가 될 가능성이 클 것이다."
-        어벤져스: 엔드게임(2019년)을 끝으로 '인피니티 사가'는 막을 내렸으니,
-        새로운 사가가 필요한 마블의 입장에서 이터널스는 새로운 서막을 알리는
-        웅대한 출발점이었다. 언론 시사회에서 처음 이터널스를 보고 접하는 느낌은
-        딱 하나였다. "이 영화는 마블판 스타워즈: 라스트 제다이(2017년)가 될
-        가능성이 클 것이다." 어벤져스: 엔드게임(2019년)을 끝으로 '인피니티
-        사가'는 막을 내렸으니, 새로운 사가가 필요한 마블의 입장에서 이터널스는
-        새로운 서막을 알리는 웅대한 출발점이었다. 언론 시사회에서 처음 이터널스를 보고 접하는 느낌은
-        딱 하나였다. "이 영화는 마블판 스타워즈: 라스트 제다이(2017년)가 될
-        가능성이 클 것이다." 어벤져스: 엔드게임(2019년)을 끝으로 '인피니티
-        사가'는 막을 내렸으니, 새로운 사가가 필요한 마블의 입장에서 이터널스는
-        새로운 서막을 알리는 웅대한 출발점이었다.`,
+      userId: null,
       isMore: false,
+      review: {},
+      likeCount: 0,
+      isLike: "",
+      isDropdownOpen: false,
     };
+  },
+  props: {
+    propReview: Object,
+  },
+  created() {
+    const user = localStorage.getItem("user");
+    if (user) {
+      this.userId = JSON.parse(localStorage.getItem("user")).user.id;
+    }
+
+    axios
+      .get(
+        `api/v1/movies/${this.propReview.movie}/reviews/${this.propReview.id}`
+      )
+      .then((res) => {
+        this.review = res.data;
+        this.likeCount = res.data.like_users.length;
+        this.isLike = res.data.like_users.includes(this.userId);
+      })
+      .catch((err) => console.log(err));
+  },
+  methods: {
+    likeIt(reviewId) {
+      const user = localStorage.getItem("user");
+      if (user) {
+        const token = JSON.parse(localStorage.getItem("user")).access_token;
+        axios({
+          method: "post",
+          url: `api/v1/movies/${this.propReview.movie}/reviews/${this.propReview.id}/likes/`,
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }).then((res) => {
+          this.likeCount = res.data.like_count;
+          this.isLike = res.data.is_liked;
+          this.isClicked = true;
+          console.log(res);
+        });
+      } else {
+        window.alert("좋아요를 누르려면 로그인 하세요.");
+      }
+    },
   },
 };
 </script>
@@ -132,6 +243,11 @@ export default {
   justify-content: space-between;
 }
 
+.profile__info__header__user {
+  display: flex;
+  align-items: baseline;
+}
+
 .profile__info__name {
   display: flex;
 }
@@ -150,6 +266,19 @@ export default {
   font-size: 14px;
   color: var(--recommend-text);
 }
+
+.profile__toggle {
+  position: relative;
+}
+
+.review__dropdown {
+  position: absolute;
+}
+
+.dots__svg {
+  stroke: var(--recommend-text);
+}
+
 .comment {
   display: flex;
 }
@@ -213,6 +342,10 @@ export default {
   margin-left: 0.2rem;
 }
 
+.like__svg {
+  stroke: var(--coral);
+}
+
 .footer__input {
   background-color: var(--profile-tag);
   margin-left: 1rem;
@@ -224,5 +357,17 @@ export default {
 
 .footer__input:focus {
   outline: 1px solid gray;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: transform 0.3s;
+  transform: translateY(0px);
+  transform: opacity 1;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+  transform: translateY(-5px);
 }
 </style>
---
+++ src/components/ReviewComment.vue
@@ -0,0 +1,160 @@
+<template>
+  <div class="comment">
+    <div class="profile">
+      <div>
+        <router-link
+          :to="{
+            name: 'Profile',
+            params: { username: comment.author.username },
+          }"
+        >
+          <div class="profile__avatar">
+            <img
+              v-if="comment.author.profile.avatar"
+              :src="comment.author.profile.avatar"
+              style="height: 32px; width: 32px"
+              class="settings__form__photo__thumbnail"
+            />
+            <icon-base v-else viewBox="0 0 64 64" width="32" height="32">
+              <icon-avatar />
+            </icon-base>
+          </div>
+        </router-link>
+      </div>
+      <div class="profile__info">
+        <div class="profile__info__header">
+          <router-link
+            :to="{
+              name: 'Profile',
+              params: { username: comment.author.username },
+            }"
+          >
+            <p
+              class="profile__nickname"
+              v-text="
+                comment.author.profile.nickname
+                  ? comment.author.profile.nickname
+                  : comment.author.username
+              "
+            ></p>
+          </router-link>
+          <p class="profile__time">{{ comment.created_at.slice(0, 10) }}</p>
+        </div>
+        <p class="comment__body">{{ comment.content }}</p>
+      </div>
+    </div>
+    <div>
+      <button v-if="userId === comment.author.id" @click="deleteComment">
+        <icon-base viewBox="0 0 18 18" width="16" height="16">
+          <icon-x class="iconX" />
+        </icon-base>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import IconBase from "@/components/IconBase.vue";
+import IconAvatar from "@/components/icons/IconAvatar.vue";
+import IconX from "@/components/icons/IconX.vue";
+
+export default {
+  components: {
+    IconBase,
+    IconAvatar,
+    IconX,
+  },
+  data() {
+    return {
+      comment: {},
+      userId: null,
+    };
+  },
+  props: {
+    commentId: Number,
+    review: Object,
+  },
+  created() {
+    // const { id } = this.$route.params;
+    this.userId = JSON.parse(localStorage.getItem("user")).user.id;
+
+    axios
+      .get(
+        `api/v1/movies/${this.review.movie}/reviews/${this.review.id}/comments/${this.commentId}`
+      )
+      .then((res) => {
+        this.comment = res.data;
+      })
+      .catch((err) => console.log(err));
+  },
+  methods: {
+    deleteComment() {
+      const token = JSON.parse(localStorage.getItem("user")).access_token;
+      // const { id } = this.$route.params;
+
+      if (confirm("정말 삭제하시겠습니까?")) {
+        axios({
+          method: "delete",
+          url: `api/v1/movies/${this.review.movie}/reviews/${this.review.id}/comments/${this.commentId}/`,
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+          .then((res) => {
+            console.log(res);
+            location.reload();
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.comment {
+  display: flex;
+  justify-content: space-between;
+}
+
+.profile {
+  display: flex;
+  margin-bottom: 1rem;
+}
+
+.profile__info {
+  margin-left: 1rem;
+  width: 100%;
+}
+
+.profile__info__header {
+  display: flex;
+  align-items: center;
+}
+
+.profile__info__name {
+  display: flex;
+}
+
+.profile__nickname {
+  font-weight: 700;
+  font-size: 16px;
+  margin-right: 0.5rem;
+}
+
+.profile__id {
+  color: var(--recommend-text);
+}
+
+.profile__time {
+  font-size: 14px;
+  color: var(--recommend-text);
+}
+
+.iconX {
+  stroke: var(--scroll-bar);
+}
+</style>
