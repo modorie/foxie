bcf1e06a69ac0bf0e8a4df39dd89b8d5c5df7ed7
devpla <chaeyeonhee@kakao.com>
2021-11-25 22:39:32 +0900

    design: Update some components

---
+++ src/components/ProfileDashbaord.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>
+      {{ profile.favorite_genres }}
+    </p>
+    <p>
+      {{ profile.favorite_actors }}
+    </p>
+    <p>
+      {{ profile.favorite_directors }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    profile: Object,
+  },
+};
+</script>
+
+<style></style>
