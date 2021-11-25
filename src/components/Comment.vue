267801a6ca633834d63da4496b0384e900bc08b8
seokzin <seokzin95@gmail.com>
2021-11-26 03:01:46 +0900

    design: Add X icon in comment layout

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="container">
     <div class="comment__left">
+      <div class="modal__exit" @click="$emit('close')">
+        <icon-base viewBox="0 0 18 18" width="16" height="16">
+          <icon-x />
+        </icon-base>
+      </div>
+
       <div class="comment__author">
         <router-link
           :to="{
@@ -44,11 +50,13 @@
 import axios from "axios";
 import IconBase from "@/components/IconBase.vue";
 import IconAvatar from "@/components/icons/IconAvatar.vue";
+import IconX from "@/components/icons/IconX.vue";
 
 export default {
   components: {
     IconBase,
     IconAvatar,
+    IconX,
   },
   data() {
     return {
@@ -78,6 +86,7 @@ export default {
   justify-content: space-between;
   align-items: center;
   border-bottom: 1px solid var(--board-header);
+  position: relative;
 }
 
 .container:last-child {
@@ -132,5 +141,14 @@ export default {
 
 .like__count {
   margin-left: 0.3rem;
+}
+
+.modal__exit {
+  position: absolute;
+  right: 0rem;
+  top: 1rem;
+  display: flex;
+  justify-content: end;
+  cursor: pointer;
 }
 </style>
